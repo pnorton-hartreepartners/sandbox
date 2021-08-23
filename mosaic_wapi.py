8bc9df366f6b6655b8a79ce49237a9f7d3055fb4
Norton <PNorton@hartreepartners.com>
2021-08-23 12:33:47 +0100

    first proper commit

--- mosaic_wapi.py
+++ mosaic_wapi.py
@@ -1,20 +1,88 @@
 import requests
 import pandas as pd
+import datetime as dt
 
 SETTLES_API = 'http://settles-api.mosaic.hartreepartners.com'
 
 
+def decorate_result(f):
+    def decorated(*args, **kwargs):
+        result = f(*args, **kwargs)
+        if result.status_code != 200:
+            print(result.text)
+            #result.raise_for_status()
+            return pd.DataFrame()
+        return pd.DataFrame(result.json())
+    return decorated
+
+
+@decorate_result
 def get_settles_ts(host, instrument_key, exchange):
     url = f'{host}/settles/api/v1/getSettlementTS/{instrument_key}?exchange={exchange}'
-    result = requests.get(url)
-    if result.status_code != 200:
-        print(result.text)
-        result.raise_for_status()
-    return pd.DataFrame(result.json())
+    return requests.get(url)
+
+
+@decorate_result
+def get_symbols(host, exchange):
+    url = f'{host}/settles/api/v1/getSymbols/{exchange}'
+    return requests.get(url)
+
+
+def build_instrument_key(symbol, forward_date):
+    forward_date_for_instrument_key = forward_date.strftime('%Y%m')
+    blank_space = ' '
+    return symbol + blank_space + forward_date_for_instrument_key
+
+
+def build_symbol_df(host, exchange, symbol, forward_dates):
+    dfs = pd.DataFrame()
+    for forward_date in forward_dates:
+        instrument_key = build_instrument_key(symbol=symbol,
+                                              forward_date=forward_date)
+        df = get_settles_ts(host=host,
+                            instrument_key=instrument_key,
+                            exchange=exchange)
+        if not df.empty:
+            df['symbol'] = symbol
+            df['forward_date'] = forward_date
+            dfs = pd.concat([dfs, df], axis='rows')
+    if not dfs.empty:
+        dfs = dfs[['symbol', 'date', 'forward_date', 'value']]
+        dfs.sort_values(['symbol', 'date', 'forward_date', 'value'], inplace=True)
+    return dfs
+
+
+def build_from_curves_df(host, curves, start_date, periods):
+    dfs = pd.DataFrame()
+    for curve in curves:
+        exchange, symbol = curve
+        forward_dates = pd.date_range(start=start_date,
+                                      periods=periods,
+                                      freq='MS')
+        df = build_symbol_df(host, exchange, symbol, forward_dates)
+        dfs = pd.concat([dfs, df], axis='rows')
+    return dfs
 
 
 if __name__ == '__main__':
-    instrument_key = 'JKM 202110'
-    exchange = 'ICE'
-    df = get_settles_ts(host=SETTLES_API, instrument_key=instrument_key, exchange=exchange)
-    print(df.head())
+    host = SETTLES_API
+
+    cme_european_naphtha_calendar_swap = ('CME', 'UN')
+    ice_brent_1st_line_future = ('ICE', 'I')
+    curves = [cme_european_naphtha_calendar_swap, ice_brent_1st_line_future]
+
+    start_date = dt.date(2020, 10, 1)
+    periods = 6
+
+    df = build_from_curves_df(host, curves, start_date, periods)
+    print()
+
+    # exchange = 'ICE'
+    # df = get_symbols(host=host, exchange=exchange)
+    # print(df.head())
+    # print()
+
+    # instrument_key = 'JKM 202110'
+    # exchange = 'ICE'
+    # df = get_settles_ts(host=host, instrument_key=instrument_key, exchange=exchange)
+    # print(df.head())
--- settles_feed.py
+++ settles_feed.py
@@ -14,8 +14,8 @@ from
 (
 SELECT exchange, "date", field, label, "source", sum(value) as value, count(*) as curve_length
 FROM settles.values
-where exchange = 'ICE'
-and source = 'ICE'
+where exchange = '{exchange}'
+and source = '{exchange}'
 and instrument_key like '{symbol} %'
 and instrument_key < '{yearmonth}'
 
@@ -29,7 +29,7 @@ full outer join
 (
 SELECT exchange, "date", field, label, "source", sum(value) as value, count(*) as curve_length
 FROM settles.values_2
-where exchange = 'ICE'
+where exchange = '{exchange}'
 and source = 'TT'
 and instrument_key like '{symbol} %'
 and instrument_key < '{yearmonth}'
@@ -48,14 +48,14 @@ order by prod.date
 """
 
 sql_detail_template = """
-select '{symbol}' as symbol, prod.*, uat.value,
+select '{symbol}' as symbol, prod.*, uat.label, uat.value,
 prod.value - uat.value as value_diff
 from 
 (
 SELECT *
 FROM settles.values
-where exchange = 'ICE'
-and source = 'ICE'
+where exchange = '{exchange}'
+and source = '{exchange}'
 and instrument_key like '{symbol} %'
 
 and char_length(instrument_key) = {length}  --<-- exclude options here
@@ -68,7 +68,7 @@ full outer join
 (
 SELECT *
 FROM settles.values_2
-where exchange = 'ICE'
+where exchange = '{exchange}'
 and source = 'TT'
 and instrument_key like '{symbol} %'
 
@@ -80,7 +80,6 @@ and date = '{date}'
 on prod.exchange = uat.exchange
 	and prod.date = uat.date
 	and prod.field = uat.field
-	and prod.label = uat.label
 	and prod.instrument_key = uat.instrument_key
 order by prod.date
 """
@@ -90,17 +89,21 @@ class Version(Enum):
     INDICATIVE = 'Settlement-Indicative'
     FINAL = 'Settlement'
 
+class Exchange(Enum):
+    ICE = 'ICE'
+    CME = 'CME'
 
-def get_checksum(symbol, yearmonth, engine):
+
+def get_checksum(exchange, symbol, yearmonth, engine):
     length = len(symbol) + 7
     yearmonth = symbol + ' ' + yearmonth
-    sql = sql_checksum_template.format(symbol=symbol, length=length, yearmonth=yearmonth)
+    sql = sql_checksum_template.format(exchange=exchange, symbol=symbol, length=length, yearmonth=yearmonth)
     return pd.read_sql(sql, con=engine)
 
 
-def get_detail(symbol, uat_label, date, engine):
+def get_detail(exchange, symbol, uat_label, date, engine):
     length = len(symbol) + 7
-    sql = sql_detail_template.format(symbol=symbol, length=length, uat_label=uat_label, date=date)
+    sql = sql_detail_template.format(exchange=exchange, symbol=symbol, length=length, uat_label=uat_label, date=date)
     return pd.read_sql(sql, con=engine)
 
 
@@ -108,11 +111,14 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--eod_date',
                         help='format YYYY-MM-DD',
-                        required=True,
                         type=dt.date.fromisoformat)
     parser.add_argument('-r', '--report',
                         help='detail or checksum',
                         choices=['detail', 'checksum'],
+                        required=True)
+    parser.add_argument('-e', '--exchange',
+                        help='CME or ICE',
+                        choices=['CME', 'ICE'],
                         required=True)
     return parser.parse_args()
 
@@ -120,14 +126,15 @@ def get_args():
 if __name__ == "__main__":
     '''
     from the command prompt:
-    >>> python settles_feed.py -d 2021-08-12 -r checksum
-    >>> python settles_feed.py -d 2021-08-12 -r detail
+    >>> python settles_feed.py -e ICE -d 2021-08-12 -r checksum
+    >>> python settles_feed.py -e ICE -d 2021-08-12 -r detail
     '''
 
     # ============================
     # some config
 
-    symbols = ['TTF', 'UKF', 'NBP', 'G', 'EUA', 'B']
+    ice_symbols = ['TTF', 'UKF', 'NBP', 'G', 'EUA', 'B']
+    cme_symbols = ['CL', 'NG', 'HO', 'XAU']
 
     path = r'c:\temp'
     yearmonth = '202301'  # a parameter to limit forward curve on advice of mathieu; checksum only
@@ -137,6 +144,16 @@ if __name__ == "__main__":
     args = get_args()
     eod_date_sql = args.eod_date
     report_selection = args.report
+    exchange = args.exchange
+
+    symbols_dict = {Exchange.ICE.value: ice_symbols,
+                    Exchange.CME.value: cme_symbols
+                    }
+    symbols = symbols_dict[exchange]
+    # eod_date_sql = dt.date(2021, 8, 12)
+    # report_selection = 'detail'
+    # exchange = 'ICE'
+
 
     # report selection
     run_checksum = True if report_selection == 'checksum' else False
@@ -146,11 +163,11 @@ if __name__ == "__main__":
     eod_date_str = eod_date_sql.strftime('%Y%m%d')
     report_run_date_str = dt.datetime.today().strftime('%Y%m%d%H%M')
 
-    checksum_filename = 'settle_checksum_{eod_date_str}_{report_run_date}.xlsx'
-    detail_filename_dict = {Version.FINAL.value: 'settle_detail_final_{eod_date_str}_{report_run_date}.xlsx',
-                            Version.INDICATIVE.value: 'settle_detail_indicative_{eod_date_str}_{report_run_date}.xlsx'}
+    checksum_filename = 'settle_checksum_{eod_date_str}_{exchange}_{report_run_date}.xlsx'
+    detail_filename_dict = {Version.FINAL.value: 'settle_detail_final_{eod_date_str}_{exchange}_{report_run_date}.xlsx',
+                            Version.INDICATIVE.value: 'settle_detail_indicative_{eod_date_str}_{exchange}_{report_run_date}.xlsx'}
 
-    checksum_file = os.path.join(path, checksum_filename.format(eod_date_str=eod_date_str, report_run_date=report_run_date_str))
+    checksum_file = os.path.join(path, checksum_filename.format(eod_date_str=eod_date_str, exchange=exchange, report_run_date=report_run_date_str))
     eod_date_sql = eod_date_sql.strftime('%Y-%m-%d')
 
     # set up db connection
@@ -160,14 +177,14 @@ if __name__ == "__main__":
     if run_checksum:
         with pd.ExcelWriter(checksum_file) as writer:
             for symbol in symbols:
-                df = get_checksum(symbol, yearmonth, engine=engine)
+                df = get_checksum(exchange=exchange, symbol=symbol, yearmonth=yearmonth, engine=engine)
                 df.to_excel(writer, sheet_name=symbol)
 
     if run_detail:
         for version in Version:
-            detail_file = os.path.join(path, detail_filename_dict[version.value].format(eod_date_str=eod_date_str, report_run_date=report_run_date_str))
+            detail_file = os.path.join(path, detail_filename_dict[version.value].format(eod_date_str=eod_date_str, exchange=exchange, report_run_date=report_run_date_str))
 
             with pd.ExcelWriter(detail_file) as writer:
                 for symbol in symbols:
-                    df = get_detail(symbol, uat_label=version.value, date=eod_date_sql, engine=engine)
+                    df = get_detail(exchange=exchange, symbol=symbol, uat_label=version.value, date=eod_date_sql, engine=engine)
                     df.to_excel(writer, sheet_name=symbol)
