3bf298ca6ba0ead6ce277dc2673fd5967c006204
Norton <PNorton@hartreepartners.com>
2021-10-15 11:37:46 +0100

    wip

--- balsamo_option_valuation.py
+++ balsamo_option_valuation.py
@@ -1,27 +1,70 @@
 import requests
-from constants import PROD, DEV, BALSAMO, PORT, hosts
+from constants import PROD, DEV, BALSAMO, PORT, SETTLES, hosts
 
-template_url_dict = {
+balsamo_template_url_dict = {
     'getEuropeanOptionValuation': r'{host}:{port}/balsacore/balsarest/risk/getEuropeanOptionValuation',
     'getAsianOptionValuation': r'{host}:{port}/balsacore/balsarest/risk/getAsianOptionValuation',
 }
 
-payload_dict = {
+balsamo_headers_dict = {
     'User': 'PNO',
-    'Pwd': 'mypassword',
+    'Pwd': '***',
     'Company': 'Hartree Partners',
     'Commodity': 'METAL CONCENTRATES',
-    'PutCall': 'P',  # Put = Sell = S; Call = Purchase = P
-    'PriceCode': 'LMEZNCB',
-    'Strike': 6662.23,
-    'MaturityDate': '2027-01-06'
+    'Content-Type': 'application/json'
 }
 
-if __name__ == '__main__':
-    api = 'getEuropeanOptionValuation'
-    url = template_url_dict[api].format(host=hosts[BALSAMO][DEV], port=hosts[BALSAMO][PORT])
+balsamo_body_dict = {
+    'Putcall': 'P',  # Put = Sell = S; Call = Purchase = P
+    'PriceCode': 'LMEZNCB',
+    'Strike': '6662.23',
+     #'MaturityDate': '2027-01-06'
+    'MaturityDate': '06/01/2027'
+}
 
-    response = requests.post(url, json=payload_dict)
+mosaic_template_url_dict = {
+    'getOptionPricesFromVolCurves': r'{host}/settles/api/v1/{api}/{symbol}/{exchange}/{stamp}/{scheme}/{rf_rate}',
+}
+
+mosaic_url_dict = {'symbol': 'ZN',
+                   'exchange': 'LME',
+                   'stamp': '2021-10-14',
+                   'scheme': 'European',
+                   'rf_rate': 0}
+
+mosaic_header_dict = {'accept': 'application/json',
+                      'Content-Type': 'application/json'}
+
+mosaic_body_dict = {'term': '202701',
+                       'parity': 'Call',
+                       'strike': '6662.23'}
+
+mosaic_payload_dict = {}
+mosaic_payload_dict.update(mosaic_header_dict)
+mosaic_payload_dict.update(mosaic_body_dict)
+
+if __name__ == '__main__':
+    balsamo_api = 'getEuropeanOptionValuation'
+    mosaic_api = 'getOptionPricesFromVolCurves'
+    env = DEV
+
+    mosaic_url = mosaic_template_url_dict[mosaic_api].format(host=hosts[SETTLES][env],
+                                                             api=mosaic_api,
+                                                             symbol=mosaic_url_dict['symbol'],
+                                                             exchange=mosaic_url_dict['exchange'],
+                                                             stamp=mosaic_url_dict['stamp'],
+                                                             scheme=mosaic_url_dict['scheme'],
+                                                             rf_rate=mosaic_url_dict['rf_rate'])
+    response = requests.post(mosaic_url, data=mosaic_payload_dict)
+    print(response)
+
+    balsamo_url = balsamo_template_url_dict[balsamo_api].format(host=hosts[BALSAMO][env],
+                                                                port=hosts[BALSAMO][PORT])
+    params_dict = {}
+    params_dict.update(balsamo_body_dict)
+    params_dict.update(balsamo_headers_dict)
+    response = requests.get(balsamo_url, json=params_dict, verify=False)
+    print(response)
     print('hello world')
 
 
