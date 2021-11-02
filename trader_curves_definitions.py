34c557b4346fe9ff745df2422d0d7ff08d9384c5
Norton <PNorton@hartreepartners.com>
2021-11-02 16:12:17 +0000

    trader curves update

--- trader_curves_definitions.py
+++ trader_curves_definitions.py
@@ -1,3 +1,10 @@
+'''
+json file generated using this api
+http://trader-curves-api.dev.mosaic.hartreepartners.com/trader-curves/api/v1/getTraderCurveTopicDictionary/definitions
+use production definition by modifying this url
+save the file in the path location defined here
+'''
+
 import json
 from pprint import pprint as pp
 import os
