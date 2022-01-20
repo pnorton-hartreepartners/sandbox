1f02849838d825085168d2c7bf631daa767b1f81
Norton <PNorton@hartreepartners.com>
2022-01-20 14:36:51 +0000

    works now

--- chart_builder.py
+++ chart_builder.py
@@ -4,9 +4,18 @@ import requests
 import copy
 from pprint import pprint as pp
 
-url = r'https://grafana.charting.dev.mosaic.hartreepartners.com/api/dashboards/db'
-cleanup_keys = ['id', 'uid', 'title', 'version']
+# filenames
+template_filename = 'chart_template.json'  # copied verbatim from grafana; needs to incl some panels
+dashboard_filename = 'chart.json'  # modified equivalent; this can be copied directly into grafana app
+dashboard_api_filename = 'chart_api.json'  # same but with additional api details
+dashboard_api_test_filename = 'chart_api_test.json'  # for manual adjustment and testing
+xls_filename = 'chart_config.xlsx'  # the xls configuration of the new required charts
 
+# urls
+dashboard_url = r'https://grafana.charting.dev.mosaic.hartreepartners.com/api/dashboards/db'
+folder_url = r'https://grafana.charting.dev.mosaic.hartreepartners.com/api/folders'
+
+# api structures
 headers_dict = {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
@@ -15,22 +24,24 @@ headers_dict = {
 
 dashboard_api_dict = {
     'dashboard': {
-        'title': 'crude',
+        'title': None,  # populate this
         'tags': ['none'],
         'timezone': 'browser',
         'schemaVersion': 16,
         'version': 0,
-        'refresh': '25s',
+        'refresh': '125s',
     },
-    'folderUid': '1ei4hE17z',
+    'folderUid': None,  # populate this
     'message': 'PN changes',
     'overwrite': True
 }
 
+# required config data
+cleanup_keys = ['id', 'uid', 'title', 'version']
 product_columns = ['currency', 'unit', 'legend', 'axis']
-panel_columns = ['title']  # 'seasonal'
-panel_type = 'mosaic-grafana-panel'
-panel_type = 'timeseries'  # for seasonal
+panel_columns = ['title']
+panel_type_is_seasonal = {True: 'timeseries',
+                          False: 'mosaic-grafana-panel'}
 
 
 def clean_the_sheets(workbook):
@@ -71,11 +82,17 @@ def build_expressions(df):
     return expressions
 
 
-def build_dashboard(xls_file, dashboard_template):
+def build_dashboard(xls_file, dashboard_template, number=None):
     # templates from grafana
     panel_template = dashboard_template['panels'][0]
     target_template = panel_template['targets'][0]
     product_template = target_template['products'][0]
+
+    # remove id's from the old template
+    panel_template = clean_template(panel_template, keys=cleanup_keys)
+
+    # =====================================================
+    # build data from xls
 
     # user configured requirements
     worksheets = pd.read_excel(io=xls_file, sheet_name=None)
@@ -98,8 +115,13 @@ def build_dashboard(xls_file, dashboard_template):
     products_df.set_index(keys='product_id', drop=True, inplace=True)
     expressions_df.set_index(keys='expression_id', drop=True, inplace=True)
 
-    # build new dicts
-    selection_df = panels_df.iloc[:1]  # only while were playing/fixing/building
+    # =====================================================
+    # build new dictionaries
+
+    # create fewer panels if we're testing or developing
+    selection_df = panels_df if not number else panels_df.iloc[:number]
+
+    # build data for each panel and update panels_content
     panels_content = build_from_template(selection_df, panel_template, panel_columns)
     for panel_id, panel in selection_df.iterrows():
         products_content = build_products_content_for_panel(products_df, expressions_df,
@@ -114,8 +136,12 @@ def build_dashboard(xls_file, dashboard_template):
         panel_content = panels_content[panel_id]
         # in our model we only have one target
         panel_content['targets'] = [target_content]
+
         # this is not in the xls config
+        panel_type = panel_type_is_seasonal[panel['seasonal'] > 0]
         panel_content['type'] = panel_type
+        # and i own these id's which controls whether i update current or create new
+        panel_content['id'] = panel_id
 
     # add the panels to the dashboard
     dashboard_dict = copy.deepcopy(dashboard_template)
@@ -140,36 +166,67 @@ def build_products_content_for_panel(products_df, expressions_df, panel_id, prod
 
 
 if __name__ == '__main__':
-    build_dashboard_selection = False
+    # user config
+    folder_uid = '1ei4hE17z'
 
-    # folders = requests.get("https://grafana.charting.dev.mosaic.hartreepartners.com/api/folders", headers=headers_dict,
-    #                        verify=False)
-    # print(folders.content.decode('utf-8'))
+    dashboard_details = {
+        'crude': {'id': 42,
+                  'uid': 'SETQfq17z',
+                  }
+        }
+
+    # user selection
+    build_dashboard_selection = True
+    get_folder_details_selection = False
+    run_api_selection = True
+    dashboard_title = 'crude'
+    refresh_interval = ''
+
+    # created but not used at the moment
+    dashboard_id = dashboard_details[dashboard_title]['id']
+    dashboard_uid = dashboard_details[dashboard_title]['uid']
+
+    # just to validate the additional api details
+    if get_folder_details_selection:
+        folders = requests.get(folder_url, headers=headers_dict, verify=False)
+        print(folders.content.decode('utf-8'))
 
     # build a json comparable to the grafana dashboard settings json
-    filename = f'chart.json'
     if build_dashboard_selection:
         # user configuration is in this xls
-        xls_file = 'chart_config.xlsx'
         # template copied from grafana app
-        dashboard_template = get_template('chart_template.json')
+        dashboard_template = get_template(template_filename)
+        # not sure why it needs an id
         dashboard_template = clean_template(dashboard_template, keys=cleanup_keys)
         # update it based on user requirements
-        db = build_dashboard(xls_file, dashboard_template)
+        db = build_dashboard(xls_filename, dashboard_template)
+        db['title'] = dashboard_title
+        db['refresh'] = refresh_interval
         # save it
-        with open(filename, 'w') as f:
+        with open(dashboard_filename, 'w') as f:
             json.dump(db, f)
     else:
-        with open(filename) as f:
+        with open(dashboard_filename) as f:
             db = json.load(f)
 
-    # add this to the template from here
-    # https://grafana.com/docs/grafana/latest/http_api/dashboard/#create--update-dashboard
-    dashboard_api_dict['dashboard'].update(db)
+    # update the api header template
+    dashboard_api_dict['dashboard']['title'] = dashboard_title
+    dashboard_api_dict['folderUid'] = folder_uid
     pp(dashboard_api_dict)
+
+    #  and then add the dashboard json
+    dashboard_api_dict['dashboard'].update(db)
+
+    # print it and save it for review/debugging
+    with open(dashboard_api_filename, 'w') as f:
+        json.dump(dashboard_api_dict, f)
 
     # call the api
     dashboard_api_dict_json = json.dumps(dashboard_api_dict)
-    rr = requests.post(url, headers=headers_dict, data=dashboard_api_dict_json, verify=False)
-    print(rr)
-    print(rr.content.decode('utf-8'))
+    if run_api_selection:
+        rr = requests.post(dashboard_url, headers=headers_dict, data=dashboard_api_dict_json, verify=False)
+        print(rr)
+        print(rr.content.decode('utf-8'))
+
+    print()
+
--- chart_test_api.py
+++ chart_test_api.py
@@ -1,45 +1,13 @@
 import json
 import requests
-from pprint import pprint as pp
-
-url = r'https://grafana.charting.dev.mosaic.hartreepartners.com/api/dashboards/db'
-
-headers_dict = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJrIjoiaHJ6M1B2TUQ5NmZOMFltMkN0TXZSdmlYVlNuYW9YUEkiLCJuIjoiYXBpYWNjZXNzIiwiaWQiOjF9'
-}
-
-dashboard_api_dict = {
-    'dashboard': {
-        #'id': '24',
-        #'uid': None,
-        'title': 'mytest',
-        'tags': ['no tags'],
-        'timezone': 'browser',
-        'schemaVersion': 16,
-        'version': 0,
-        'refresh': '25s',
-    },
-    'folderId': 1,
-    'folderUid': '1ei4hE17z',
-    'message': 'PN changes',
-    'overwrite': True
-}
+from chart_builder import dashboard_url, headers_dict, dashboard_api_test_filename
 
 if __name__ == '__main__':
-
-    # use the template instead
-    filename = 'chart_template.json'
-    with open(filename) as f:
+    with open(dashboard_api_test_filename) as f:
         db = json.load(f)
 
-    # update the json from the docs
-    dashboard_api_dict['dashboard'].update(db)
-
-    pp(dashboard_api_dict)
-
-    dashboard_api_dict_json = json.dumps(dashboard_api_dict)
-    rr = requests.post(url, headers=headers_dict, data=dashboard_api_dict_json, verify=False)
+    # call the api
+    dashboard_api_dict_json = json.dumps(db)
+    rr = requests.post(dashboard_url, headers=headers_dict, data=dashboard_api_dict_json, verify=False)
     print(rr)
     print(rr.content.decode('utf-8'))
