c019a2036a96dd8669218f84f79964b78452d44b
Norton <PNorton@hartreepartners.com>
2022-02-07 18:30:44 +0000

    file faffing

--- fot_email_attachment_analysis.py
+++ fot_email_attachment_analysis.py
@@ -10,6 +10,7 @@ folder_new = r'\\gateway\hetco\P003\Tasks\Excel Reports\Gasoline New Reports\Rep
 file_type_old = '.xls'
 file_type_new = '.xlsb'
 
+
 def get_stems_from_filename(filenames):
     return list(zip(*[file.split('.') for file in filenames]))[0]
 
@@ -24,20 +25,24 @@ stems = get_stems_from_filename(pdfs)
 attachment_stems = [stem[:-11] for stem in stems]
 attachment_df = pd.DataFrame(data=attachment_stems, columns=['attachment'])
 
+# get all filenames in old-style (otto) xls location
 xlss_old = os.listdir(folder_old)
 xlss_old_stems = get_stems_from_filename(xlss_old)
 xls_old_df = pd.DataFrame(data=xlss_old_stems, columns=['xls_old'])
 
+# get all filenames in new-style xls location
 xlss_new = os.listdir(folder_new)
 xlss_new_stems = get_stems_from_filename(xlss_new)
 xls_new_df = pd.DataFrame(data=xlss_new_stems, columns=['xls_new'])
 
+# merge everything
 df1 = pd.merge(attachment_df, xls_old_df, how='left', left_on='attachment', right_on='xls_old')
 df2 = pd.merge(df1, xls_new_df, how='left', left_on='attachment', right_on='xls_new')
 
 # if neither source matches the attachment name then we have a problem
 df2['bad'] = df2[['xls_old', 'xls_new']].isna().all(axis='columns')
 
+# create the full clickable pathname
 mask = ~df2['xls_old'].isna()
 df2.loc[mask, 'xls_filepath'] = df2.loc[mask, 'xls_old']\
     .apply(lambda x: prepend_pathname(folder_old, x, file_type_old))
