9bda8ada3d802f9c7fe1835c06f49f88c142a951
Norton <PNorton@hartreepartners.com>
2022-02-14 12:10:27 +0000

    check for faiure

--- mosaic_api_grafana_check.py
+++ mosaic_api_grafana_check.py
@@ -12,12 +12,18 @@ with open('mosaic_chart_examples.json') as file:
     chart_examples = json.load(file)
 payload = chart_examples['platts']
 
+failure = False
 for api_name in grafana_api_names:
     url, params, method = prepare_inputs_for_api(api_name, env=PROD)
 
     if method == 'get':
         result, df, error = get_any_api(url, params)
         print('\n==========\n', api_name, '\n\n', result, '\n\n', df.head(), '\n\n==========\n')
+        if not(result.status_code == 200 and not df.empty and not error):
+            failure = True
     elif method == 'post' and api_name == 'getTraderCurveTS':
         result = post_any_api(url, payload=payload)
+        if not(len(result) > 0):
+            failure = True
         print('\n==========\n', api_name, '\n\n', pd.DataFrame(result).head(), '\n\n==========\n')
+    print('\n==========\n==========', f'\nall api calls succeeded: {not failure}')
