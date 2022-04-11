314da323521a656b6054f0a51f8c034e7aae9ed8
Norton <PNorton@hartreepartners.com>
2022-04-11 14:28:31 +0100

    analyst data views

---
+++ analyst_data_views_.py
@@ -0,0 +1,16 @@
+import os
+# import analyst_data_views
+
+os.environ['CRATE_HOST'] = 'ttda.storage.mosaic.hartreepartners.com:4200'
+#os.environ['CRATE_HOST'] = 'ttda.cratedb-dev-cluster.mosaic.hartreepartners.com:4200'
+
+from analyst_data_views.common.db_flattener import getFlatMappedDF
+from analyst_data_views.common.converter import addUnits
+from analyst_data_views.common.db_flattener import getFlatRawDF
+
+source = "iea-mods"
+raw_data = getFlatRawDF(source)
+print('cheese')
+pass
+
+
